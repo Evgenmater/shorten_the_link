85b57f3144e21d49a6f825a5d46af5bd8607cf65
Евгений Хлебнев <hlebnev@yandex.ru>
2023-12-29 02:09:59 +0300

    s

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import os
 
-LOCAL_HOST = os.getenv('LOCAL_HOST')
+LOCAL_HOST = os.getenv('LOCAL_HOST', default='http://localhost/')
 
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_YA', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
